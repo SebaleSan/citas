ec9a6202c14a5003f46ef2518d1bdefe16a1ed84
Sebalesan <se.santanderv@duocuc.cl>
2026-04-16 19:17:32 -0400

    se crea controller medico end point para listar y crear nuevo medico

---
+++ src/main/java/com/semana2/citas/controller/MedicoController.java
@@ -0,0 +1,41 @@
+package com.semana2.citas.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.semana2.citas.dto.MedicoRequestDTO;
+import com.semana2.citas.dto.MedicoResponseDTO;
+import com.semana2.citas.service.MedicoService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    private final MedicoService medicoService;
+
+    public MedicoController(MedicoService medicoService) {
+        this.medicoService = medicoService;
+    }
+    
+    @GetMapping
+    public ResponseEntity<?> obtenerTodos() {
+        return ResponseEntity.ok(medicoService.obtenerTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<MedicoResponseDTO> crearMedico(@Valid @RequestBody MedicoRequestDTO medicoRequest) {
+        MedicoResponseDTO medicoCreado = medicoService.crearMedico(medicoRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(medicoCreado);
+    } 
+
+    
+    
+}
